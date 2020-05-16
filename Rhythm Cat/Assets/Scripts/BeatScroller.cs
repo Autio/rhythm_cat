f2b0313649c3f74bef9128603b7defa950a62660
Petri Autio <petri.mikael.autio@gmail.com>
2020-05-16 14:27:44 +0300

    Boilerplate

---
+++ Rhythm Cat/Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+    public float beatTempo;
+    public bool hasStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 120 = two units per second
+        beatTempo = beatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0, beatTempo * Time.deltaTime, 0);
+        }
+    }
+}
